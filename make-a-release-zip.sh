b257f246ac1c6570d8a1c009aa6370794bd531e0
TsT <tst2005@gmail.com>
2020-02-06 13:43:32 +0100

    support multiple major version

--- make-a-release-zip.sh
+++ make-a-release-zip.sh
@@ -1,15 +1,27 @@
 #!/bin/sh
 
+if [ $# -eq 0 ]; then
+	echo >&2 "Usage: $0"' 0.17|0.18'
+	exit 1
+fi
+
 # test the zip command ?
 # use p7zip ?
 
-ZIPNAME="$(jq -r < info.json '.name + "_" + .version')"
+pkgdir="Packaging/$1"
+infojson=info.json
+
+ZIPNAME="$(jq -r < "$pkgdir/$infojson" '.name + "_" + .version')"
+if [ -z "$ZIPNAME" ]; then
+	echo >&2 "ERROR: Unable to get the mod's name and version. Something wrong with the content of 'info.json'."
+	exit 1
+fi
 if [ -e "$ZIPNAME.zip" ]; then
 	echo >&2 "ERROR: zip $ZIPNAME.zip already exists"
 	exit 1
 fi
 [ -e "$ZIPNAME" ] || ln -s . "$ZIPNAME"
 
-find "$ZIPNAME/" -type f ! -path '*/.*' ! -name '*.zip' |sort | zip "$ZIPNAME.zip" -@
-rm "$ZIPNAME"
-#zip --help
+cp -a "Packaging/$1/$infojson" "./info.json"
+find "$ZIPNAME/" -type f ! -path '*/.*' ! -name '*.zip' ! -name '[A-Z_]*' ! -path '*/[A-Z]*' ! -name 'info???*.json' |egrep -v '(\.old|\.new|changelog\.md)' |sort | zip "$ZIPNAME.zip" -@
+rm "$ZIPNAME" "./info.json"
